2e11dcd779771196752188b5500799554ddad88e
Danil Markov <danilmarkov88@gmail.com>
2023-09-15 01:50:43 +0400

    LabWork01 - done

--- main.py
+++ main.py
@@ -1,16 +1,55 @@
-# This is a sample Python script.
+from flask import Flask, render_template, request
+import pandas as pd
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+df = pd.read_csv('../AgeDataset-V1.csv')
+df.to_csv('../AgeDataset-V1.csv', index=None)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+@app.route('/table')
+def table():
+    data = pd.read_csv('../AgeDataset-V1.csv')
+
+    max_rows = max(data.axes[0])
+    max_columns = len(data.axes[1])
+    empty_cells = data.isna().sum().sum()
+    filled_cells = data.count().sum()
+
+    info_df = pd.DataFrame({
+        "Название столбца": data.columns,
+        "Тип данных": data.dtypes,
+    })
+
+    if request.args.get("row_from") is None:
+        return render_template('table.html',
+                               max_rows=max_rows,
+                               max_columns=max_columns,
+                               info_df=info_df,
+                               empty_cells=empty_cells,
+                               filled_cells=filled_cells)
+
+    row_from = 1 if request.args.get("row_from") is None else int(request.args.get("row_from")) - 1
+    row_to = 1 if request.args.get("row_to") is None else int(request.args.get("row_to"))
+    column_from = 1 if request.args.get("column_from") is None else int(request.args.get("column_from")) - 1
+    column_to = 1 if request.args.get("column_to") is None else int(request.args.get("column_to"))
+
+    result = data.iloc[row_from:row_to, column_from:column_to]
+
+    return render_template('table.html',
+                           tables=[result.to_html()],
+                           titles=[''],
+                           max_rows=max_rows,
+                           info_df=info_df,
+                           max_columns=max_columns,
+                           empty_cells=empty_cells,
+                           filled_cells=filled_cells)
+
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=int("5000"))
